98adf4ff1b40b988165487067508fedda14c2838
Susant Shrestha <sushantsrain@gmail.com>
2017-06-18 11:02:32 +0545

    scroll-bar added

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 $(document).ready(function() {
-  var id;
-  showSection();
+  // variables
+  var id; // variable for initializing active nav btn href value
+  var activeContent;
+  var scrollBar;
+  var scrollBarNav;
+  var dh = $(document).height(); // get document height
+  var sbnh;
+
+  // section-page positioning and navigation functions start
+  showSection(); // shows the section relative to active bio-content
+  positionScrollBar(); // sets the height of the scroll bar...
   $("nav ul li a").click(function() {
     if (id != $(this).attr("href")) {
       $(".active").removeClass("active");
       var active = $(this);
       active.addClass("active");
       saveActiveBtn(active);
-      translateMenuBar();
       showSection();
+      positionScrollBar();
+      translateMenuBar();
     }
   });
 
@@ -17,8 +27,9 @@ $(document).ready(function() {
     var active = $("a[href='#about-page']").not(this);
     active.addClass("active");
     saveActiveBtn(active);
-    translateMenuBar();
     showSection();
+    positionScrollBar();
+    translateMenuBar();
   });
 
   function showSection() {
@@ -50,15 +61,46 @@ $(document).ready(function() {
 
   function translateMenuBar() {
     var nav = $("nav")
-    var top = nav.css("top");
-    var sectionContent = $("section div[id*='content']");
+    var navTop = nav.css("top");
     // console.log(top);
-    if (top == "0px") {
+    if (navTop == "0px") {
       nav.css({"top": "100%", "marginTop": "-90px"});
-      sectionContent.css("top", "0");
+      activeContent.css("top", "0");
+      scrollBar.css("top", "0");
     } else {
       nav.css({"top": "0", "marginTop": "0"});
-      sectionContent.css("top", "90px");
+      activeContent.css("top", "90px");
+      scrollBar.css("top", "90px");
     }
   }
+
+  function positionScrollBar() {
+    activeContent = $(id+" div[id*=-content]"); // find active section content
+    rowContainer = $(activeContent).children("div.row-container");
+    scrollBar = $(activeContent).children("div.scroll-bar"); // find active section scroll bar element
+    scrollBarNav = $(scrollBar).children("div.scroll-bar-nav"); // find active scroll bar navigation element
+
+    activeContent.outerHeight(dh-90);
+    var ah = activeContent.outerHeight(); // get active section content height
+    var rh = rowContainer.outerHeight();
+    scrollBar.height(ah); // set the scroll bar height
+    var sbh = scrollBar.height(); // get scroll bar height
+    sbnh = (sbh/rh)*100; // set scroll bar navigation height
+    scrollBarNav.height(sbnh+"%");
+    // console.log(dh+" "+ah+" "+rh+" "+sbh+" "+sbnh);
+  }
+  // section-page navigation functions end
+
+
+  //scroll-bar parallax functions start
+  $(activeContent).on("scroll", function() {
+    positionScrollBar();
+    var st = activeContent.scrollTop();
+    var speed = sbnh/100;
+    st = speed * st;
+    scrollBarNav.css({
+      "top": st
+    });
+  });
+
 });
